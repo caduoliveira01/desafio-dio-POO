ee9b3ea10012c7c37b5e91e0f79214b00c2c0724
caduoliveira01 <caduolivera17@gmail.com>
2025-08-06 18:34:10 -0300

    desafio finalizado

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,32 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso extends Conteudo {
+
+
+    private Integer cargaHoraria;
+
+    public Curso() {
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "Titulo" + getTitulo()+
+                "Descrição" + getDescricao()+
+                "cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
